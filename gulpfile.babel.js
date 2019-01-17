660d4bea96fa408fdaad8971566f8c49cecd6c4d
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-01-17 12:26:29 +0300

    Add gulp task to compress theme to .zip

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,18 @@
 import { src, dest, watch, series } from 'gulp';
 import del from 'del';
+import zip from 'gulp-zip';
+import info from './package.json';
 
-export const clean = () => del(['dist']);
-export const copy = () => src(['src/**/*']).pipe(dest('dist'));
+export const clean = () => del(['dist','bundled']);
+export const copy = () => src(['src/**/*']).pipe(dest(`dist/${info.name}`));
 export const watchForChanges = () => watch(['src/**/*'], copy);
 
+export const compress = () => {
+	return src([`dist/${info.name}/**/*`])
+		.pipe(zip(`${info.name}.zip`))
+		.pipe(dest('bundled'));
+};
+
 export const dev = series(clean, copy, watchForChanges)
-export const build = series(clean, copy)
+export const build = series(clean, copy, compress)
 export default dev;
